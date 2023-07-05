3f385247ee4a97fb091d0587f819a5a8fd7ced80
ivan kevra <vanyakevra@gmail.com>
2023-07-05 14:16:20 +0300

    add view image model

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {photos, PhotoType} from "../../state/state";
 import style from './style.module.css'
 
 export const Gallery = () => {
+
+    const [viewMode, setViewMode] = useState<boolean>(false)
+    const [image, setImage] = useState<string>('')
+
+
+    const getImage = (imageUrl: string) => {
+        setImage(imageUrl)
+        setViewMode(true)
+    }
+
     return (
-        <section className={style.gallery}>
-            <h1 className={style.title}>Gallery</h1>
-            <div className={style.galleryList}>
-                {photos.map((photo: PhotoType) => {
-                    return (
-                        <a href={photo.imageUrl} className={style.galleryItem}>
-                            <div className={style.galleryItemHover}>Посмотреть</div>
-                            <img src={photo.imageUrl} alt={photo.title}/>
-                        </a>
-                    )
-                })}
+        <>
+            <div className={viewMode ? `${style.model} ${style.open}` : style.model}>
+                <img src={image}/>
             </div>
-        </section>
+            <section className={style.gallery}>
+                <h1 className={style.title}>Gallery</h1>
+                <div className={style.galleryList}>
+                    {photos.map((photo: PhotoType, index) => {
+                        return (
+                            <div
+                                className={style.galleryItem}
+                                key={index}
+                                onClick={() => getImage(photo.imageUrl)}>
+                                <div className={style.galleryItemHover}>Посмотреть</div>
+                                <img src={photo.imageUrl} alt={photo.title}/>
+                            </div>
+                        )
+                    })}
+                </div>
+            </section>
+        </>
+
     );
 };
 
--- src/state/state.ts
+++ src/state/state.ts
@@ -1,7 +1,4 @@
-import {v1} from "uuid";
-
 export type PhotoType = {
-    id: string
     title: string
     description: string
     imageUrl: string
@@ -9,151 +6,126 @@ export type PhotoType = {
 
 export const photos: PhotoType[] = [
     {
-        id: v1(),
         title: 'Croatia river',
         description: 'Plitvice Lakes National Park in Croatia. Plitvice Lakes National Park is a must add to your Croatia itinerary.',
         imageUrl: 'https://i.pinimg.com/236x/d2/ab/ba/d2abbacebd30efb029a506054887ab14.jpg'
     },
     {
-        id: v1(),
         title: '10 of The Best Waterfalls in The World',
         description: 'There’s nothing quite as mesmerising as standing in complete awe in front of one of Mother Nature’s most astonishing waterfalls.',
         imageUrl: 'https://i.pinimg.com/564x/a4/46/4e/a4464e2e6c69a6bb7fe7fdd942788908.jpg'
     },
     {
-        id: v1(),
         title: 'Sunset',
         description: '"Every sunset is an opportunity to reset."– Richie Norton',
         imageUrl: 'https://i.pinimg.com/564x/09/47/db/0947db859f8bbec51f4f85d030c59b03.jpg'
     },
     {
-        id: v1(),
         title: 'White River on Mount Rainier',
         description: 'The internet\'s largest community of landscape photographers and Earth lovers. [This subreddit is now private. Click here to find out why we have...',
         imageUrl: 'https://i.pinimg.com/564x/ce/63/89/ce638990f9771cb735de8654c4f51ad9.jpg'
     },
     {
-        id: v1(),
         title: 'Garden in Italy ',
         description: 'With a brief to create a garden in the Italian countryside in tune with its surroundings, the designer Stuart Barfoot combined subtle landscaping with romantic plantings in a wildlife-rich scheme that captivates the senses.',
         imageUrl: 'https://i.pinimg.com/564x/ce/63/89/ce638990f9771cb735de8654c4f51ad9.jpg'
     },
     {
-        id: v1(),
         title: 'River in Italy ',
         description: '16 of the Best Places to Visit in Italy.',
         imageUrl: 'https://i.pinimg.com/564x/a3/bd/99/a3bd99ff636a9cd1c51b07c65f1aae60.jpg'
     },
     {
-        id: v1(),
         title: 'Stunning sunset photography',
         description: 'Nature landscape road photography',
         imageUrl: 'https://i.pinimg.com/564x/86/65/68/866568383b5911ba53265a980c43e271.jpg'
     },
     {
-        id: v1(),
         title: 'Air castle',
         description: 'Air castle',
         imageUrl: 'https://i.pinimg.com/564x/07/7f/aa/077faa00278e86e9a5f59461888d715e.jpg'
     },
     {
-        id: v1(),
         title: 'Croatia river',
         description: 'Plitvice Lakes National Park in Croatia. Plitvice Lakes National Park is a must add to your Croatia itinerary.',
         imageUrl: 'https://i.pinimg.com/236x/d2/ab/ba/d2abbacebd30efb029a506054887ab14.jpg'
     },
     {
-        id: v1(),
         title: '10 of The Best Waterfalls in The World',
         description: 'There’s nothing quite as mesmerising as standing in complete awe in front of one of Mother Nature’s most astonishing waterfalls.',
         imageUrl: 'https://i.pinimg.com/564x/a4/46/4e/a4464e2e6c69a6bb7fe7fdd942788908.jpg'
     },
     {
-        id: v1(),
         title: 'Sunset',
         description: '"Every sunset is an opportunity to reset."– Richie Norton',
         imageUrl: 'https://i.pinimg.com/564x/09/47/db/0947db859f8bbec51f4f85d030c59b03.jpg'
     },
     {
-        id: v1(),
         title: 'White River on Mount Rainier',
         description: 'The internet\'s largest community of landscape photographers and Earth lovers. [This subreddit is now private. Click here to find out why we have...',
         imageUrl: 'https://i.pinimg.com/564x/ce/63/89/ce638990f9771cb735de8654c4f51ad9.jpg'
     },
     {
-        id: v1(),
         title: 'Garden in Italy ',
         description: 'With a brief to create a garden in the Italian countryside in tune with its surroundings, the designer Stuart Barfoot combined subtle landscaping with romantic plantings in a wildlife-rich scheme that captivates the senses.',
         imageUrl: 'https://i.pinimg.com/564x/ce/63/89/ce638990f9771cb735de8654c4f51ad9.jpg'
     },
     {
-        id: v1(),
         title: 'River in Italy ',
         description: '16 of the Best Places to Visit in Italy.',
         imageUrl: 'https://i.pinimg.com/564x/a3/bd/99/a3bd99ff636a9cd1c51b07c65f1aae60.jpg'
     },
     {
-        id: v1(),
         title: 'Stunning sunset photography',
         description: 'Nature landscape road photography',
         imageUrl: 'https://i.pinimg.com/564x/86/65/68/866568383b5911ba53265a980c43e271.jpg'
     },
     {
-        id: v1(),
         title: 'Air castle',
         description: 'Air castle',
         imageUrl: 'https://i.pinimg.com/564x/07/7f/aa/077faa00278e86e9a5f59461888d715e.jpg'
     },
 
     {
-        id: v1(),
         title: 'Croatia river',
         description: 'Plitvice Lakes National Park in Croatia. Plitvice Lakes National Park is a must add to your Croatia itinerary.',
         imageUrl: 'https://i.pinimg.com/236x/d2/ab/ba/d2abbacebd30efb029a506054887ab14.jpg'
     },
     {
-        id: v1(),
         title: '10 of The Best Waterfalls in The World',
         description: 'There’s nothing quite as mesmerising as standing in complete awe in front of one of Mother Nature’s most astonishing waterfalls.',
         imageUrl: 'https://i.pinimg.com/564x/a4/46/4e/a4464e2e6c69a6bb7fe7fdd942788908.jpg'
     },
     {
-        id: v1(),
         title: 'Sunset',
         description: '"Every sunset is an opportunity to reset."– Richie Norton',
         imageUrl: 'https://i.pinimg.com/564x/09/47/db/0947db859f8bbec51f4f85d030c59b03.jpg'
     },
     {
-        id: v1(),
         title: 'White River on Mount Rainier',
         description: 'The internet\'s largest community of landscape photographers and Earth lovers. [This subreddit is now private. Click here to find out why we have...',
         imageUrl: 'https://i.pinimg.com/564x/ce/63/89/ce638990f9771cb735de8654c4f51ad9.jpg'
     },
     {
-        id: v1(),
         title: 'Garden in Italy ',
         description: 'With a brief to create a garden in the Italian countryside in tune with its surroundings, the designer Stuart Barfoot combined subtle landscaping with romantic plantings in a wildlife-rich scheme that captivates the senses.',
         imageUrl: 'https://i.pinimg.com/564x/ce/63/89/ce638990f9771cb735de8654c4f51ad9.jpg'
     },
     {
-        id: v1(),
         title: 'River in Italy ',
         description: '16 of the Best Places to Visit in Italy.',
         imageUrl: 'https://i.pinimg.com/564x/a3/bd/99/a3bd99ff636a9cd1c51b07c65f1aae60.jpg'
     },
     {
-        id: v1(),
         title: 'Stunning sunset photography',
         description: 'Nature landscape road photography',
         imageUrl: 'https://i.pinimg.com/564x/86/65/68/866568383b5911ba53265a980c43e271.jpg'
     },
     {
-        id: v1(),
         title: 'Air castle',
         description: 'Air castle',
         imageUrl: 'https://i.pinimg.com/564x/07/7f/aa/077faa00278e86e9a5f59461888d715e.jpg'
     },
-
 
 
 ]
